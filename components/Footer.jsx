ba20a33464df22b604c8161a8fbb95daa4f20af9
Rodrigo Menezes <rodrigo_menezes91@hotmail.com>
2023-02-14 13:33:25 -0300

    site

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,7 +12,7 @@ export default function () {
         ">
             <div className='flex flex-col md:flex-row justify-center items-center w-[320px] md:w-[1000px] md:gap-20'>
                 <Image src={logo} alt='logo empresa' className='w-44 md:w-44 mt-10' />
-                <div className=" flex md:gap-28 w-[320px] md:w-[1200px]">
+                <div className=" flex flex-col md:flex-row md:gap-28 w-[320px] md:w-[1200px]">
                     <div className="flex flex-col items-center justify-center pt-10">
                         <a href="/" className="text-lg font-bold mb-5">
                             Início
