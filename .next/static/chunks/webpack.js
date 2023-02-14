74a8dda0ba28945da568bb2494a3b9cdc870fa21
Rodrigo Menezes <rodrigo_menezes91@hotmail.com>
2023-02-14 13:42:30 -0300

    site

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7989acfc90b05d49"; }
+/******/ 		__webpack_require__.h = function() { return "045527de9abd4428"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Local.jsx
+++ components/Local.jsx
@@ -4,7 +4,7 @@ import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 export default function Local() {
     return (
-        <div id='comochegar' className='flex flex-col md:w-auto h-full items-center md:pt-20 pb-20 -z-10 text-center'>
+        <div id='comochegar' className='flex flex-col md:w-auto h-full items-center md:pt-20s -z-10 text-center'>
             <div className='flex flex-col justify-center items-center'>
                 <h1 className='text-3xl md:text-6xl p-10 text-segundo-tema-site drop-shadow-dark1'>
                     Localização
@@ -21,7 +21,7 @@ export default function Local() {
             </div>
             <div>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14785.03269610517!2d-45.0531622!3d-22.1161301!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cb4b151f38312d%3A0xbc9b8357ec703993!2sShopping%20Ant%C3%B4nio%20Dutra!5e0!3m2!1spt-BR!2sbr!4v1676296114356!5m2!1spt-BR!2sbr" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
-                    className='w-screen md:w-screen md:h-[500px]'
+                    className='w-screen md:w-screen h-[500px]'
                 >
                 </iframe>
             </div>
